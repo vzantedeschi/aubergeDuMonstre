4dd3b56314b0ded9330705807df6126b5c944970
Cyprien Cattelain <cyprien.cattelain@insa-lyon.fr>
2014-01-09 14:53:36 +0100

    version intermediary

--- Serveur_Python/connexion_passerelle.py
+++ Serveur_Python/connexion_passerelle.py
@@ -10,22 +10,28 @@ connexion_avec_passerelle = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_passerelle.connect((hote, port))
 print("Connexion établie avec le serveur sur le port {}".format(port))
 
-trame = "A55A3B0510020024000000042024"
-trame2 = "A55A0B05000000000021CC072024"
-trameEteindre = "A55A3B05700000000021CC0730A4"
-trameIDMoodleAllumer = "A55A3B0500000000FF9F1E042024"
-trameIDMoodleEteindre = "A55A3B0570000000FF9F1E042024"
-trame1 = trameIDMoodleAllumer.encode()
+trameIntOn = "A55A0B05100000000021CC073044"
+trameIntOff = "A55A0B05300000000021CC073064"
+
+trameIDMoodleAllumer = "A55A5B0510000000FF9F1E033044"
+
+trameIDMoodleEteindre = "A55A5B0530000000FF9F1E006064"
+
+trame1 = trameIDMoodleEteindre.encode()
 connexion_avec_passerelle.send(trame1)
 
 while 1:
     # Client est de type socket
-    msg_recu = connexion_avec_passerelle.recv(224)
+    msg_recu = connexion_avec_passerelle.recv(28)
     # Peut planter si le message contient des caractères spéciaux
     msg_recu = msg_recu.decode()
     ident = msg_recu[16:24]
     print ident
     if ident == "0021CC07" :
+        print("Reçu {}".format(msg_recu))
+    if ident == "00893386" :
+        print("Reçu {}".format(msg_recu))
+    if ident == "00054155" :
         print("Reçu {}".format(msg_recu))
         
         
