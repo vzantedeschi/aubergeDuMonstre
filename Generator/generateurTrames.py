c3f02697a1e7249c78875c52edfc6a4922d44190
Cyprien Cattelain <cyprien.cattelain@insa-lyon.fr>
2014-01-11 09:42:50 +0100

    minimal modifications on variables for more modularity

--- Generator/generateurTrames.py
+++ Generator/generateurTrames.py
@@ -3,48 +3,64 @@
 class generateurTrames():
 
     def __init__(self, identifiants):
-    	fic_id = open(identifiants,"r")
-    	liste = fic_id.readlines()
-    	self.enteteTrames = 'A55A0B'
-    	fic_id.close()
+        fic_id = open(identifiants,"r")
+        liste = fic_id.readlines()
+        
+        ###modifsCyp
+        syncBytes = "A55A"
+        h_seq_length = "0B"
+        self.enteteTrames = syncBytes+h_seq_length
+        ###
+        
+        fic_id.close()
 
-	for capt in liste:
-		type, id = capt.split()
+        for capt in liste:
+            type, id = capt.split()
 
-		if (type == 'fenetre') :
-			self.fenetre = id
+            if (type == 'fenetre') :
+                self.fenetre = id
 
-		elif (type == 'interrupteur') :
-			self.interrupteur = id
+            elif (type == 'interrupteur') :
+                self.interrupteur = id
 
-		elif (type == 'presence') :
-			self.presence = id
+            elif (type == 'presence') :
+                self.presence = id
 
     #Generation des trames de l'interrupteur
     def pressON(self) :
-    	message = "051000000000"
-    	queueTrame = "2024"
-    	return self.enteteTrames + message + queueTrame
+        org = "05"
+        dataBytes = "10000000"
+        idBytes = self.interrupteur
+        message = org+dataBytes+idBytes
+        status = "20"
+        checksum = "24"
+        queueTrame = status+checksum
+        return self.enteteTrames + message + queueTrame
 
     def pressOFF(self) :
-    	message = "053000000000"
-    	queueTrame = "2024"
-    	return self.enteteTrames + message + queueTrame
+        org = "05"
+        dataBytes = "30000000"
+        idBytes = self.interrupteur
+        message = org+dataBytes+idBytes
+        status = "20"
+        checksum = "24"
+        queueTrame = status+checksum
+        return self.enteteTrames + message + queueTrame
 
     def getCapteurs(self) :
-    	print "liste des capteurs en memoire :"
-    	print "capteur d'ouverture fenetre ", self.fenetre
-    	print "capteur de detection de presence ", self.presence
-    	print "interrupteur sans fils ", self.interrupteur 
+        print "liste des capteurs en memoire :"
+        print "capteur d'ouverture fenetre ", self.fenetre
+        print "capteur de detection de presence ", self.presence
+        print "interrupteur sans fils ", self.interrupteur 
 
 if __name__ == "__main__" :
-	print '#################TESTS UNITAIRES##################'
+    print '#################TESTS UNITAIRES##################'
 
-	gen = generateurTrames("identifiants.txt")
+    gen = generateurTrames("identifiants.txt")
 
-	print '\n***Test 1 : Lecture fichier des identifiants***'
-	gen.getCapteurs()
+    print '\n***Test 1 : Lecture fichier des identifiants***'
+    gen.getCapteurs()
 
-	print '\n***Test 2 : interrupteur : allumer/eteindre***'
-	print "allumer => ", gen.pressON()
-	print "eteindre => ", gen.pressOFF()
+    print '\n***Test 2 : interrupteur : allumer/eteindre***'
+    print "allumer => ", gen.pressON()
+    print "eteindre => ", gen.pressOFF()
