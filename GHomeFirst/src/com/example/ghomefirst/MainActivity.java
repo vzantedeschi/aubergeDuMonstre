24c4c6a70860cb3be5e7a2f6a14d9ba208d3b7f0
vzantedeschi <vzantedeschi@gmail.com>
2014-01-28 13:02:47 +0100

    added a socket for PC-tablet communication

--- GHomeFirst/src/com/example/ghomefirst/MainActivity.java
+++ GHomeFirst/src/com/example/ghomefirst/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		new Thread(new ClientThread()).start();
 	}
 
 	@Override
