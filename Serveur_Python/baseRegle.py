41aa8800815dde6134bfb2424e84e6171f46b525
Cyprien Cattelain <cyprien.cattelain@insa-lyon.fr>
2014-01-28 23:22:31 +0100

    baseRegle

---
+++ Serveur_Python/baseRegle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Commande():
+    def __init__(self):
+
+        ### ICI EXAMINER LA BDD ###
+
+        ### Il me faut le format des informations de sortie pour savoir
+        ### sur quoi faire une condition
+        if (True):
+            print 'Présence détectée'
+            self.type = 'PRES'
+            # je met d'autre informations qu'on pourrait trouver dans la BDD
+            self.piece = 'piece1'
+            self.heure = 'heure1'
+        else:
+            print 'autre type de commande'
+            self.type = 'OTHER'
