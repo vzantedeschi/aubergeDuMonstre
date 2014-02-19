129df7e9c82da8ac18d3428431375cef5f3720cf
vzantedeschi <vzantedeschi@gmail.com>
2014-02-19 10:29:28 +0100

    template pour la description des pièces

--- Serveur_Python/commAppli.py
+++ Serveur_Python/commAppli.py
@@ -14,6 +14,39 @@ db_connec = connect('GHome_BDD')
 # \ ! / Monkey patching mongoengine to make json dumping easier
 Document.to_dict = lambda s : json.loads(s.to_json())
 
+
+def etat_to_tuples(piece_id):
+	etat = tables.Etat.objects(piece_id=piece_id).first()
+	print etat
+	piece = tables.Piece.objects(piece_id=piece_id).first()
+	print piece
+	rid = "Fermés"
+	if etat.rideauxOuverts : rid = "Ouverts"
+
+	inc = "Non déclenchée"
+	if etat.antiIncendieDeclenche : inc = "Déclenchée"
+
+	clim = "Eteinte"
+	if etat.climActivee : rid = "Active"
+
+	vol = "Fermés"
+	if etat.voletsOuverts : vol = "Ouverts"
+
+	prise = "Fermés"
+	if etat.priseDeclenchee : prise = "Ouverts"
+
+	result = { "couples": [ { "image" : "hotel.png", "nom":"Piece" , "valeur": piece.name },
+							{ "image" : "hotel.png", "nom":"Température" , "valeur": etat.temperature },
+							{ "image" : "hotel.png", "nom":"Humidité" , "valeur": etat.humidite },
+							{ "image" : "rideaux.png", "nom":"Rideaux" , "valeur": rid },
+							{ "image" : "hotel.png", "nom":"Climatisation" , "valeur": piece.name },
+							{ "image" : "hotel.png", "nom":"Prise intelligente" , "valeur": prise },
+							{ "image" : "hotel.png", "nom":"Antincendie" , "valeur": inc },
+							{ "image" : "hotel.png", "nom":"Volets" , "valeur": vol }
+							]
+			}
+	return result
+
 @app.route('/')
 def hello():
     return render_template('index.html')
@@ -46,6 +79,11 @@ def get_etats():
 	etats = [e.to_dict() for e in tables.Etat.objects.order_by('+piece_id')]
 	reponse = dict(ok=True, result=etats)
 	return json.dumps(reponse)
+
+@app.route('/surveillance/etat/<piece_id>')
+def get_etat_piece(piece_id):
+	etat = etat_to_tuples(piece_id)
+	return json.dumps(etat)
 
 @app.route('/surveillance/<perso_id>')
 def ignore(perso_id):
--- Serveur_Python/static/js/main.js
+++ Serveur_Python/static/js/main.js
@@ -8,6 +8,22 @@ function alertNon() {
 	$('#notification').hide();
 }
 
+/* Handlebars */
+loadTemplate = function(template_id) {
+    var source = $(template_id).html();
+    return Handlebars.compile(source);
+}
+
+Handlebars.registerHelper('breaklines', function(text) {
+    text = Handlebars.Utils.escapeExpression(text);
+    text = preprocessText(text);
+    return new Handlebars.SafeString(text);
+});
+
+Handlebars.registerHelper('fixedDecimal', function(number) {
+  return number.toFixed(4);
+});
+
 $(document).ready(function() { 
     $('#notification').hide();
 })
