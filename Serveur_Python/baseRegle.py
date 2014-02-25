3420873e92c686cac4f8dec82f80b5219a727596
Eloise Skoric <eloise.skoric@insa-lyon.fr>
2014-02-25 15:07:05 +0100

    Ajout controle prise

--- Serveur_Python/baseRegle.py
+++ Serveur_Python/baseRegle.py
@@ -487,6 +487,8 @@ def realisationDemandeAction(actionneurType, actionType):
             allumeLum()
         elif actionneurType == 'PORTE':
             ouvrePiece()
+        elif actionnerType == 'PRISE':
+        	allumeInt()
     else:
         if actionneurType == 'VOL':
             fermeVolet()
@@ -500,6 +502,8 @@ def realisationDemandeAction(actionneurType, actionType):
             eteintLum()
         elif actionneurType == 'PORTE':
             fermePiece()
+        elif actionnerType == 'PRISE':
+        	eteintInt()
 
 def commande():
     global rfidDetected
